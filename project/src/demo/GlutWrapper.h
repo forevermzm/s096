e5cf1a7f2723b64d5891ba1483402e71b9709c9a
Andre Kessler <akessler@mit.edu>
2014-01-29 10:24:44 -0500

    Working Windows OOP version

---
+++ project/src/demo/GlutWrapper.h
@@ -0,0 +1,53 @@
+#include <glload/gl_3_0.h>
+#include <glload/gll.hpp>
+#include <GL/freeglut.h>
+
+#include <cstdint>
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+class Shaders;
+
+class GlutWrapper {
+  static void displayCallback();
+  static void reshapeCallback( int theWidth, int theHeight );
+  static void keyboardCallback( unsigned char key, int x, int y );
+  static void mouseCallback( int button, int state, int x, int y );
+	static void openglDebug( GLenum source, GLenum type, GLuint id, 
+													 GLenum severity, GLsizei length,
+													 const GLchar* message, GLvoid* userParam );
+	Shaders *_shaders;
+	void GlutWrapper::initVertexBuffers();
+protected:
+  int _width, _height;
+  int _windowId;
+  std::string _windowTitle;
+	GLuint _positionBufferObject;
+	GLuint _vao;
+	GLuint _program;
+  static GlutWrapper *_instance;
+public:
+	enum Mode { DEBUG, NDEBUG };
+  GlutWrapper();
+  GlutWrapper( Mode debugMode );
+	GlutWrapper::GlutWrapper( const std::string &title );
+	GlutWrapper( const std::string &title, 
+               Mode debugMode = Mode::NDEBUG );
+  ~GlutWrapper();
+
+  void init( int argc, char **argv, int theWidth, int theHeight, Shaders *shaders );
+  void createWindow();
+	void run();
+  static GlutWrapper* getInstance();
+
+  inline int width() const { return _width; }
+  inline int height() const { return _height; }
+
+  virtual void display() = 0;
+  virtual void reshape( int theWidth, int theHeight ) = 0;
+  virtual void keyboard( unsigned char key, int x, int y );
+  virtual void mouse( int button, int state, int x, int y );
+private:
+  Mode _debugMode;
+};
